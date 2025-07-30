930230a39d609d7e499ed82918592149fcfef567
Makenna Meyer <mmeyer3@ncsu.edu>
2025-07-29 23:39:32 -0400

    done minus API and docker

--- plumber.R
+++ plumber.R
@@ -9,20 +9,45 @@
 
 library(plumber)
 
-# Reading in the data
-#diabetes_data <- read.csv("diabetes_binary_health_indicators_BRFSS2015.csv")
+#Reading in the data
+diabetes_data <- read.csv("diabetes_binary_health_indicators_BRFSS2015.csv")
 
-# Fitting the best model
+#Selecting response and 3 chosen explanatory variables
+#Converting categorical variables from numeric variables to factor variables
+diabetes_select <- diabetes_data |>
+  rename("diabetes_resp" = Diabetes_binary) |>
+  rename("bmi" = BMI) |>
+  rename("phys_activity" = PhysActivity) |>
+  rename("age" = Age) |>
+  mutate(diabetes_resp = factor(diabetes_resp, levels = c(0, 1), labels = c("No diabetes", "Prediabetes or diabetes"))) |>
+  mutate(phys_activity = factor(phys_activity, levels = c(0, 1), labels = c("No physical activity", "Physical activity"))) |>
+  mutate(age = factor(age, levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13), labels = c("Age 18-24", "Age 25-29", "Age 30-34", "Age 35-39", "Age 40-44", "Age 45-49", "Age 50-54", "Age 55-59", "Age 60-64", "Age 65-69", "Age 70-74", "Age 75-79", "Age 80 or older"))) |>
+  mutate(HvyAlcoholConsump = as.factor(HvyAlcoholConsump)) |>
+  mutate(AnyHealthcare = as.factor(AnyHealthcare))
 
+#Fitting the model using logistic regression
+LR1_fit <- glm(diabetes_resp ~ phys_activity + bmi + age + HvyAlcoholConsump + AnyHealthcare, data = diabetes_select, family = binomial)
 
 #* @apiTitle Plumber Example API
 #* @apiDescription Plumber example description.
 
 #* Creating predictions using the best model
-#* @param msg The message to echo
+#* @param bmi BMI
+#* @param phys_activity Physical activity in the last 30 days
+#* @param age age group
+#* @param HvyAlcoholConsump heavy alcohol consumption
+#* @param AnyHealthcare any healthcare coverage
 #* @get /pred
-function(msg = "") {
-    list(msg = paste0("The message is: '", msg, "'"))
+function(bmi, phys_activity, age) {
+    predictor_data <- tibble(as.numeric(bmi), 
+                             as.factor(phys_activity), 
+                             as.factor(age), 
+                             as.factor(HvyAlcoholConsump),
+                             as.factor(AnyHealthcare))
+    
+    model_prediction <- predict(LR1_fit, predictor_data)
+    
+    list(model_prediction = as.character(model_prediction))
 }
 
 #* Displaying author and GitHub information
